cdd1e951f33cd13db0343a578a6db67aefe5fd3e
Mike McNeil <mike@balderdash.co>
2014-07-01 07:30:35 -0500

    Formatting, added comment explaining whats going on in there

--- api/controllers/PetController.js
+++ api/controllers/PetController.js
@@ -6,18 +6,20 @@
  */
 
 module.exports = {
-	index: function (req,res) {
+
+  find: function(req, res) {
     Pet.native(function(err, collection) {
       if (err) return res.serverError(err);
 
+      // In this example, I'm using a native query to do a projection (i.e. SQL "SELECT")
+      // But you can do anything here - this collection is directly from the raw mongo driver.
       collection.find({}, {
         name: true
-      }).toArray(function (err, results) {
+      }).toArray(function(err, results) {
         if (err) return res.serverError(err);
-        console.log('->',results);
+        console.log('->', results);
         return res.ok(results);
       });
     });
   }
 };
-
